7f714986feed74b3e54bff0722c66abde1373488
Andrew Pipo <atpipo2026@gmail.com>
2023-05-28 19:05:12 -0400

    Added do-while loop for playing again

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,39 @@
+import java.util.Scanner; // import scanner for user input
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // initialize scanner for users input
+        Scanner playerOne = new Scanner(System.in);
+        Scanner playerTwo = new Scanner(System.in);
+        Scanner playAgain = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // initialize variables
+        String playerOneMove = ""; // string to store the move for player one
+        String playerTwoMove = ""; // string to store the move for player two
+        String playAgainInput = ""; // string to store if players want to play again
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // do while loop in place to ensure the game continues to run until an N is received
+        do {
+
+            // clears user input for playing again to ensure another response can be received
+            playAgainInput = "";
+
+            // loops while the user has not inputted proper input and ensures that user will input something valid
+            while (!(playAgainInput.equalsIgnoreCase("Y") || playAgainInput.equalsIgnoreCase("N"))) {
+                // output and obtain user input for if they want to play again
+                System.out.println("Would you like to play again? [Y or N]?");
+                // assign user input to variable
+                playAgainInput = playAgain.nextLine();
+                // checks for if it is a valid input of Y or N or y or n
+                if (!(playAgainInput.equalsIgnoreCase("Y") || playAgainInput.equalsIgnoreCase("N"))) {
+                    // if not Y, y, N, or n, prompt error message and user tries again
+                    System.out.println("You entered " + playAgainInput);
+                    System.out.println("That is an invalid input. Please try again and enter Y or N when prompted.");
+                }
+            }
+            // will rerun loop if user inputs Y or y
+        } while (playAgainInput.equalsIgnoreCase("Y"));
     }
 }
